5c2bc1803129c09baebc1fab71b66df19e6b845b
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-07-07 11:35:16 +0100

    Update chat.php

--- views/chat.php
+++ views/chat.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,94 +16,27 @@
 <body>
     <div class="wrapper">
         <section class="chat-area">
-            <header>
-                <a href="" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-                
-                    <img src="ayo.jpg" alt="">
-                           
-                <div class="details">
-                    <span>
-                           Ayo Queen
-                          </span>
-                    <p>Active Now</p>
-                </div>
-
+            <header id="mega">
+           
+             
+              
+         
             </header>
-            <div class="chat-box">
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Lorem ipsum dolor sit amet consectetur </p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="ayo.jpg" alt="">
-                    <div class="details">
-                        <p>Lorem inventore at magnam possimus nobis! Magni veniam nostrum necessitatibus cum saepe?</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Lorem ipsum dolor sit amet consectetur </p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="ayo.jpg" alt="">
-                    <div class="details">
-                        <p>Lorem inventore at magnam possimus nobis! Magni veniam nostrum necessitatibus cum saepe?</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Lorem ipsum dolor sit amet consectetur </p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="ayo.jpg" alt="">
-                    <div class="details">
-                        <p>Lorem inventore at magnam possimus nobis! Magni veniam nostrum necessitatibus cum saepe?</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Lorem ipsum dolor sit amet consectetur </p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="ayo.jpg" alt="">
-                    <div class="details">
-                        <p>Lorem inventore at magnam possimus nobis! Magni veniam nostrum necessitatibus cum saepe?</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Lorem ipsum dolor sit amet consectetur </p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="ayo.jpg" alt="">
-                    <div class="details">
-                        <p>Lorem inventore at magnam possimus nobis! Magni veniam nostrum necessitatibus cum saepe?</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Lorem ipsum dolor sit amet consectetur </p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="ayo.jpg" alt="">
-                    <div class="details">
-                        <p>Lorem inventore at magnam possimus nobis! Magni veniam nostrum necessitatibus cum saepe?</p>
-                    </div>
-                </div>
+            <div class="chat-box" id = "out-in-message">
+              
             </div>
         
-            <form action="chat.php" method="GET" class="typing-area">
-                <input type="text" name="message" id="" placeholder="Type a message">
-                <button name="send"><i class="fab fa-telegram-plane"></i></button>
+
+            <form action="" onsubmit= "return false"  class="typing-area" id="typing-area">
+             <p id="myText"></p>
+             <p id="myText-1"></p>
+             <input type="text" name="message" id="message" placeholder="Type a message">
+             <button name="send" id="sendBtn"><i class="fab fa-telegram-plane"></i></button>          
             </form>
         </section>
     </div>
+    <script src="../javascript/test.js"></script>
+   
 </body>
 
 </html>
